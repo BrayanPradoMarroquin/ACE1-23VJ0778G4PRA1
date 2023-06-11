0090a391e948949b33b9af13252ba020f58dacb7
Eduardo Agustin <3006840880101@ingenieria.usac.edu.gt>
2023-06-10 23:41:17 -0600

    Pintar edificios
    Se pintan los edificios :D

--- Edificios/Edificios.ino
+++ Edificios/Edificios.ino
@@ -31,21 +31,66 @@ byte buffer[8][16] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 
 int tamanio = 3;
 
-int edificios[16][2] = { { 0, 3 },
-                         { 4, 3 },
-                         { 10, 3 } };
+int edificios[16][2] = { { 1, 3 },
+                         { 2, 3 },
+                         { 3, 0 },
+                         { 4, 0 },
+                         { 5, 0 },
+                         { 6, 0 },
+                         { 7, 0 },
+                         { 8, 4 },
+                         { 9, 0 },
+                         { 10, 0 },
+                         { 11, 0 },
+                         { 12, 0 },
+                         { 13, 0 },
+                         { 14, 0 },
+                         { 15, 0 },
+                         { 0, 0 },
+                        };
 
-void pintaredificio() {
+void pintarLED(int x, int y) {
+    digitalWrite(14  + y, LOW);
+    digitalWrite(22 + x, HIGH);
+    delayMicroseconds(1100);
+    digitalWrite(14  + y, HIGH);
+    digitalWrite(22 + x, LOW);
+}
+
+void crearedificios() {
   //CON DRIVER
-  for (int k = 0; k < 16; k++) {
-    if (edificios[k][1] != 0) {
-      for (int i = 0; i <= edificios[k][1]; i++)
-        matriz.setLed(0, 8 - i, edificios[k][0], true);
+  for(int k = 0; k < 16; k++){
+    if (edificios[k][1] != 0){
+      for(int i = 0; i < edificios[k][1]; i++){
+        buffer[7 - i][edificios[k][0]] = 1;
+      }
     }
   }
 }
 
+  void pintaredificio(){
+    //CON DRIVER
+    for (int i = 0; i < 8; i++){
+      for (int j = 0; j < 8; j++){
+        matriz.setLed(0, i, j, buffer[i][j] == 1 ? true : false);
+      }
+    }
+      
+    //SIN DRIVER
+    for(int k = 0; k < 3; k++){
+      for(int i = 0; i < 8; i++){
+        for(int j = 8; j < 16; j++)
+          if(buffer[i][j] == 1){
+            pintarLED(j - 8, i);
+          }else{
+            delayMicroseconds(50);
+          }
+      }
+    }
+  }
+
 void loop() {
+  crearedificios();
   pintaredificio();
-  delayMicroseconds(1);
+  delayMicroseconds(0.5);
 }
