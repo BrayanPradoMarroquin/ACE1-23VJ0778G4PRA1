af7dae78689ebb5cbc1c7923880047221baf6a45
Kelly901 <3416152852103@ingenieria.usac.edu.gt>
2023-06-11 21:37:12 -0600

    estaditiscas_prototipo

--- Menu/estadistica/estadistica.ino
+++ Menu/estadistica/estadistica.ino
@@ -6,7 +6,7 @@
 
 LedControl matriz = LedControl(DIN, CLK, LOAD, 1);
 
-void setup(){
+void setup() {
   matriz.shutdown(0, false);
   matriz.setIntensity(0, 8);
   for (int i = 14; i <= 21; i++)
@@ -27,6 +27,15 @@ void setup(){
   pinMode(10, INPUT);
   //Pausa juego pin -> 10
 }
+
+double punteos[5][4] = { { 1, 3, 0, 0 },
+                         { 2, 4, 0, 0 },
+                         { 3, 5, 0, 0 },
+                         { 4, 6, 0, 0 },
+                         { 5, 4, 0, 0 } };
+
+int totalpunteo = 0;
+
 
 byte buffer[8][16] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
                        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -91,25 +100,58 @@ unsigned long buttonPressStartTime10 = 0;
 unsigned long buttonPressStartTime10P = 0;
 
 void pintarLED(int x, int y) {
-  // put your main code here, to run repeatedly:
-
-  //digitalWrite(PIN,HIGH)
   digitalWrite(14 + y, LOW);
   digitalWrite(22 + x, HIGH);
-  delay(1);
+  delayMicroseconds(1100);
   digitalWrite(14 + y, HIGH);
   digitalWrite(22 + x, LOW);
 }
 
+int temp1 = 0;
+
+void calcularP() {
+  Serial.begin(9600);
+  for (int i = 0; i < 5; i++) {
+    totalpunteo = totalpunteo + punteos[i][1];
+    Serial.println(totalpunteo);
+  }
+
+  for (int j = 0; j < 5; j++) {
+    retorno((punteos[j][1] / totalpunteo) * 100);
+    punteos[j][2] = temp1;
+    Serial.println(punteos[j][2]);
+  }
+}
+
+void retorno(double porcentaje) {
+  if (porcentaje > 0 && porcentaje <= 13) {
+    temp1 = 1;
+  } else if (porcentaje > 13 && porcentaje <= 25) {
+    temp1 = 2;
+  } else if (porcentaje > 25 && porcentaje <= 38) {
+    temp1 = 3;
+  } else if (porcentaje > 38 && porcentaje <= 50) {
+    temp1 = 4;
+  } else if (porcentaje > 50 && porcentaje <= 53) {
+    temp1 = 5;
+  } else if (porcentaje > 53 && porcentaje <= 75) {
+    temp1 = 6;
+  } else if (porcentaje > 75 && porcentaje <= 88) {
+    temp1 = 7;
+  } else if (porcentaje > 88 && porcentaje <= 100) {
+    temp1 = 8;
+  }
+}
 
 void configuracion() {
-  Serial.begin(9600);
-  potenciometroVelocidad = map(analogRead(A0), 0, 1024, 0, 7);
-  potenciometroVidas = map(analogRead(A1), 0, 1024, 0, 7);
+  potenciometroVelocidad = map(analogRead(A0), 0, 1024, 0, 8);
+  potenciometroVidas = map(analogRead(A1), 0, 1024, 0, 8);
+  velocidad = 200 - (potenciometroVelocidad * 15);
+  vidaInicial = 3 + potenciometroVidas;
 
   for (int i = 1; i < 3; i++) {
     for (int j = 0; j < 8; j++) {
-      if (7 - j < potenciometroVelocidad)
+      if (7 - j < punteos[0][2])
         buffer[j][i] = 1;
       else
         buffer[j][i] = 0;
@@ -118,7 +160,34 @@ void configuracion() {
 
   for (int i = 4; i < 6; i++) {
     for (int j = 0; j < 8; j++) {
-      if (8 - j < potenciometroVidas)
+      if (7 - j < punteos[1][2])
+        buffer[j][i] = 1;
+      else
+        buffer[j][i] = 0;
+    }
+  }
+
+  for (int i = 7; i < 9; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (7 - j < punteos[2][2])
+        buffer[j][i] = 1;
+      else
+        buffer[j][i] = 0;
+    }
+  }
+
+  for (int i = 10; i < 12; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (7 - j < punteos[3][2])
+        buffer[j][i] = 1;
+      else
+        buffer[j][i] = 0;
+    }
+  }
+
+  for (int i = 13; i < 15; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (7 - j < punteos[4][2])
         buffer[j][i] = 1;
       else
         buffer[j][i] = 0;
@@ -154,6 +223,12 @@ void configuracion() {
   mostrarMatriz();
 }
 
-void loop(){
+int temp = 0;
+
+void loop() {
+  if (temp == 0) {
+    calcularP();
+    temp++;
+  }
   configuracion();
 }
